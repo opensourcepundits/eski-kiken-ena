201b12aba5ca1d7efcffbf4097d95d33a8df6b3e
Y-Noor <noor_yadallee@hotmail.com>
2026-02-20 16:42:44 +0400

    vercel

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,9 +3,12 @@ import postgres from 'postgres';
 import { env } from '$env/dynamic/private';
 import * as schema from './schema';
 
-const client = postgres(env.POSTGRES_URL || 'postgres://postgres:postgres@localhost:5432/eke', {
-	prepare: false,
-	ssl: process.env.NODE_ENV === 'production' ? 'require' : false
-});
+const client = postgres(
+	env.DATABASE_URL || env.POSTGRES_URL || 'postgres://postgres:postgres@localhost:5432/eke',
+	{
+		prepare: false,
+		ssl: process.env.NODE_ENV === 'production' ? 'require' : false
+	}
+);
 
 export const db = drizzle(client, { schema });
